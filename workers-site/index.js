0afb0002763061074bac9682b5f102f19db749f0
yung00000 <yung0000@gmail.com>
2025-10-05 11:24:11 +0800

    Update wrangler.toml to refine asset configuration and enhance document specifications

---
+++ workers-site/index.js
@@ -0,0 +1,18 @@
+// workers-site/index.js
+import { getAssetFromKV, mapRequestToAsset } from '@cloudflare/kv-asset-handler'
+
+addEventListener('fetch', event => {
+  event.respondWith(handleEvent(event))
+})
+
+async function handleEvent(event) {
+  try {
+    return await getAssetFromKV(event)
+  } catch (e) {
+    let pathname = new URL(event.request.url).pathname
+    return new Response(`"${pathname}" not found`, {
+      status: 404,
+      statusText: 'not found'
+    })
+  }
+}
